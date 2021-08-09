3ebfe1c1445b6fcf5f7cf3f0d6cd0d898cca45cb
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-09 08:01:22 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -38,7 +38,7 @@ help_callback_filter = filters.create(lambda _, __, query: query.data.startswith
 def help_answer(client, callback_query):
     chat_id = callback_query.from_user.id
     message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split('+')[-1])
+    msg = int(callback_query.data.split('+')[1])
     client.edit_message_text(chat_id=chat_id,    message_id=message_id,
         text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
     )
