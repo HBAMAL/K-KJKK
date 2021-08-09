6fd9d118b8acf2c37489e2af47f80a4edc544bd9
HB <85717438+HBAMAL@users.noreply.github.com>
2021-08-09 09:18:12 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -81,7 +81,7 @@ def about(pos):
         url = "https://github.com/DamienSoukara/FSub-Heroku"
         button = [
            [InlineKeyboardButton(text = '❤️CHANNNEL❤️', url="https://t.me/telsabots")],
-           [InlineKeyboardButton(text = '🧑🏼‍💻DEV🧑🏼‍💻', url="https://t.me/alluaddict")],[InlineKeyboardButton(text = '◀️', callback_data = f"about+{pos-1}")],
+           [InlineKeyboardButton(text = '🧑🏼‍💻DEV🧑🏼‍💻', url="https://t.me/alluaddict")],[InlineKeyboardButton(text = '◀️', callback_data = f"about+{pos-+1}")],
         ]
     else:
         button = [
